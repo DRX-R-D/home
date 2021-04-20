56bc215f9787fffd1ed8f1eb052ed7d32e621c37
홍성준 <freevue@drx.gg>
2021-04-20 15:07:57 +0900

    [new]: react router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            Hello About
+          </Route>
+          <Route path="/users">
+            Hello Users
+          </Route>
+          <Route path="/">
+            Hello Home
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
